0dc1ad093b33bdd7fa6332bdb11613a2e09586be
clementlefevre <clement.san@gmail.com>
2015-11-15 22:08:52 -0300

    2208

--- ch02/classifying_seeds.py
+++ ch02/classifying_seeds.py
@@ -4,8 +4,10 @@ import utils
 import load
 import logging
 import numpy as np
+import scipy as sp
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
+import fig4_5_withSciKit_Learn
 
 
 logging.basicConfig(format=' %(message)s', level=logging.INFO)
@@ -21,12 +23,15 @@ class SeedsClassification():
 
     def main(self):
     	global features,labels 
-    	self.knn()
-    
+    	self.knn_with_kFold()
+    	self.knn_manual()
+    	fig4_5_withSciKit_Learn.drawFigure_no_normalization(features,labels,1,[0,2])
+    	features = (features - features.mean(0)) / features.std(0)
+    	fig4_5_withSciKit_Learn.drawFigure_no_normalization(features,labels,1,[0,2])
+
 
     #Create a model on knn with One neighbours and and calculate the accuracy on testing data with cross_validation on 5 folds
-    def knn(self):
-    	global features,labels 
+    def knn_with_kFold(self):
     	classifier = KNeighborsClassifier(n_neighbors=1)
     	kFold = cross_validation.KFold(len(features), n_folds = 5, shuffle = True)
     	accuracy = 0.0
@@ -38,7 +43,34 @@ class SeedsClassification():
     		accuracy = np.mean(prediction==labels[testing])
     		globalaccuracy+=accuracy
     		print("accuracy {0:.1%}").format(accuracy)
-    	print("globalaccuracy {0:.1%}").format(globalaccuracy/5)
-    	
+    	print("global accuracy {0:.1%}").format(globalaccuracy/5)
+
+
+
+    def knn_manual(self):
+
+    	classifier = KNeighborsClassifier(n_neighbors=1)
+    	training = np.ones(len(features), bool)
+    	training[-10:] = False
+    	testing = ~training
+
+    	split_idx = 0.90*len(features)
+    	shuffled = sp.random.permutation(list(range(len(features))))
+        testing = sorted(shuffled[:split_idx])
+
+        training = sorted(shuffled[split_idx:])
+    	print "training : {0}".format(training)
+    	print "testing : {0}".format(testing)
+    	model = classifier.fit(features[training],labels[training])
+    	prediction = classifier.predict(features[testing])
+    	accuracy = np.mean(prediction==labels[testing])
+    	print("accuracy manual : {0:.1%}").format(accuracy)
+
+
+    def drawKnnWithSklearn(self):
+    	pass
+
+
+		
 if __name__ == '__main__':
 	SeedsClassification().main()
