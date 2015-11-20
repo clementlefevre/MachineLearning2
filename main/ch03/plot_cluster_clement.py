7661147bdb7f01de092ab1fc4e872914479b2534
clementlefevre <clement.san@gmail.com>
2015-11-20 11:54:46 -0300

    Loop on iteration for Kmeans()

--- main/ch03/plot_cluster_clement.py
+++ main/ch03/plot_cluster_clement.py
@@ -31,13 +31,33 @@ num_clusters = 3
 seed = 2
 
 
+
+def createNormalizedDataSets():
+    xw1 = norm(loc=0.3, scale=.15).rvs(20)
+    yw1 = norm(loc=0.3, scale=.15).rvs(20)
+
+    xw2 = norm(loc=0.7, scale=.15).rvs(20)
+    yw2 = norm(loc=0.7, scale=.15).rvs(20)
+
+    xw3 = norm(loc=0.2, scale=.15).rvs(20)
+    yw3 = norm(loc=0.8, scale=.15).rvs(20)
+
+    x = sp.append(sp.append(xw1, xw2), xw3)
+    y = sp.append(sp.append(yw1, yw2), yw3)
+
+    return x,y
+
 #define the scope
 rangeScope = np.arange(0,1,0.001)
-
-Xscope,Yscope = np.meshgrid(rangeScope)
+Xscope,Yscope = np.meshgrid(rangeScope, rangeScope)
 
 #create random dataset of 2 vectors, one for each word
-xDataset,yDataset = self.createNormalizedDataSets()
+xDataset,yDataset = createNormalizedDataSets()
+training = np.array(list(zip(xDataset, yDataset)))
+
+#create a testing dataset from meshgrid
+testing = np.vstack([Xscope.ravel(),Yscope.ravel()]).T
+
 
 
 class Plotter():
@@ -46,74 +66,39 @@ class Plotter():
 
         #plot the vectors
         self.plot_chart("Vectors")
-        print "vectors plotted"
-
-
         
-        km = KMeans(init='random', n_clusters=num_clusters, verbose=1,
-            n_init=1, max_iter=1,
-            random_state=seed)
-        training = np.array(list(zip(x, y)))
+        #plot the clusters iteration 
+        for i in [1,2,10]:            
+            prediction,centers  = self.predictKMeans(i)
+            predictionReshaped = prediction.reshape(Xscope.shape)
+            self.plot_chart("Iteration_"+str(i), predictionReshaped, centers)
+       
+
+    def predictKMeans(self, iteration):
+        km =  KMeans(init="random", n_clusters = num_clusters, verbose=1, \
+            n_init=1,max_iter=iteration,random_state=seed)
         km.fit(training)
-        
-        
-        
-        testing = self.convertMeshGrid(X,Y)
-
-        #create a prediction model from dataset
-        model = self.createPredictionModel(x,y , km,testing)
+        return km.predict(testing), km.cluster_centers_
 
 
-
-        self.plot_chart(x, y, "Prediction",model)
-        print "With Prediction Mesh plotted"
-
- 
-
-    def createNormalizedDataSets(self):
-        xw1 = norm(loc=0.3, scale=.15).rvs(20)
-        yw1 = norm(loc=0.3, scale=.15).rvs(20)
-
-        xw2 = norm(loc=0.7, scale=.15).rvs(20)
-        yw2 = norm(loc=0.7, scale=.15).rvs(20)
-
-        xw3 = norm(loc=0.2, scale=.15).rvs(20)
-        yw3 = norm(loc=0.8, scale=.15).rvs(20)
-
-        x = sp.append(sp.append(xw1, xw2), xw3)
-        y = sp.append(sp.append(yw1, yw2), yw3)
-
-        return x,y
-
-
-    def convertMeshGrid(self, X,Y):
-        vstack = np.vstack([X.ravel(),Y.ravel()])
-        transposed =  vstack.T
-        return transposed
-
-    def createPredictionModel(self, x,y ,km,testing, X):
-       Z = km.predict(testing)
-       return  Z.reshape(X.shape)
-
-    def plot_chart(self,title,C=None, X=None, Y=None):
+    def plot_chart(self,title,C=None, centers=None):
         plt.figure(num=None, figsize=(8, 6))
         plt.clf()
-        
+
         if C !=None:
-            pdb.set_trace()
-            plt.pcolormesh(X, Y, C, cmap=plt.cm.Blues)
+            plt.pcolormesh(Xscope, Yscope, C, cmap=plt.cm.Blues)
+            for i in range(len(centers)):
+                plt.scatter(centers[i][0],centers[i][1],s=10, color='g')
+
+
         plt.scatter(xDataset, yDataset, s=3, color='r')
         plt.title(title)
         plt.xlabel("word1")
         plt.ylabel("word2")
         plt.autoscale(tight=True)
         plt.grid(True, linestyle='-', color='0.75')
-
         plt.savefig(os.path.join(CHART_DIR, title))
-
-
         
 if __name__ == "__main__":
     Plotter().main()
-
 
