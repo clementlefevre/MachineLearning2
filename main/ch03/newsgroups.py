82a2e61d7a9e9ac265a6673661a9762b73807966
clementlefevre <clement.san@gmail.com>
2015-11-22 12:05:11 -0300

    CH03 : use scipi euclidian distance.

--- main/ch03/newsgroups.py
+++ main/ch03/newsgroups.py
@@ -6,20 +6,24 @@ import numpy as np
 import main.ch03.clustering
 
 groups = ['comp.graphics', 'comp.os.ms-windows.misc',
-'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
-'comp.windows.x', 'sci.space']
+          'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
+          'comp.windows.x', 'sci.space']
 
-training_data = sklearn.datasets.fetch_20newsgroups(subset="train",categories=groups)
+groups = ["comp.graphics"]
 
-testing_data = sklearn.datasets.fetch_20newsgroups(subset="test", categories=groups) 
+training_data = sklearn.datasets.fetch_20newsgroups(subset="train", categories=groups)
+
+testing_data = sklearn.datasets.fetch_20newsgroups(subset="test", categories=groups)
+
 
 class StemmedTfidfVectorizer(TfidfVectorizer):
-
     def build_analyzer(self):
         analyzer = super(StemmedTfidfVectorizer, self).build_analyzer()
         return lambda doc: (main.ch03.clustering.english_stemmer.stem(w) for w in analyzer(doc))
 
-vectorizer = StemmedTfidfVectorizer(min_df=1,stop_words="english", decode_error='ignore')
+
+vectorizer = StemmedTfidfVectorizer(min_df=1, stop_words="english", decode_error='ignore')
+
 
 class NewsGroups():
     def main(self):
@@ -27,35 +31,32 @@ class NewsGroups():
         self.searchNearestPost(vectorizer, "StemmedTfidf", True)
 
     def searchNearestPost(self, vectorizer, methodName, normalized=True):
-        pdb.set_trace()
-        trainingVector = vectorizer.fit_transform(training_data)
-        testingVector = vectorizer.transform(testing_data)
+        trainingVector = vectorizer.fit_transform(training_data.data)
+        testingVector = vectorizer.transform(testing_data.data)
+        # pdb.set_trace()
 
-        #let is iterate on all training Vectors and compute the distance with one test post:
+        # let is iterate on all training Vectors and compute the distance with one test post:
         bestDistance = 100
         bestPostIdx = -1
 
-       
+        testingV = testingVector.getrow(0).toarray()
         for i in range(trainingVector.shape[0]):
             trainingV = trainingVector.getrow(i).toarray()
-            testingV = testingVector.getrow(i).toarray
+
             d = self.euclidianDistance(trainingV, testingV)
             if d < bestDistance:
                 bestDistance = d
-                bestPostIdx = i
-        print "best d: {0} : {1}".format(bestDistance,training_data.data[bestPostIdxt])
 
+        print "best d: {0} : {1}".format(bestDistance, training_data.data[bestPostIdx])
 
-
-    def euclidianDistance(self, V1,V2, normalized=False):
+    def euclidianDistance(self, V1, V2, normalized=False):
         if normalized:
-            V1 = V1/ sp.linalg.norm(V1)
-            V2 = V2/ sp.linalg.norm(V2)
-        pdb.set_trace()
-        delta = V1-V2
+            V1 = V1 / sp.linalg.norm(V1)
+            V2 = V2 / sp.linalg.norm(V2)
+        # pdb.set_trace()
+        delta = V1 - V2
         return sp.linalg.norm(delta)
-        
+
+
 if __name__ == "__main__":
     NewsGroups().main()
-
-
--- main/ch03/plot_cluster_clement.py
+++ main/ch03/plot_cluster_clement.py
@@ -1,4 +1,3 @@
-
 from scipy.stats import norm
 
 import matplotlib.pyplot as plt
@@ -31,7 +30,6 @@ num_clusters = 3
 seed = 2
 
 
-
 def createNormalizedDataSets():
     xw1 = norm(loc=0.3, scale=.15).rvs(20)
     yw1 = norm(loc=0.3, scale=.15).rvs(20)
@@ -45,63 +43,59 @@ def createNormalizedDataSets():
     x = sp.append(sp.append(xw1, xw2), xw3)
     y = sp.append(sp.append(yw1, yw2), yw3)
 
-    return x,y
+    return x, y
 
-#define the scope
-rangeScope = np.arange(0,1,0.001)
-Xscope,Yscope = np.meshgrid(rangeScope, rangeScope)
+# define the scope
+rangeScope = np.arange(0, 1, 0.001)
+Xscope, Yscope = np.meshgrid(rangeScope, rangeScope)
 
-#create random dataset of 2 vectors, one for each word
-xDataset,yDataset = createNormalizedDataSets()
+# create random dataset of 2 vectors, one for each word
+xDataset, yDataset = createNormalizedDataSets()
 training = np.array(list(zip(xDataset, yDataset)))
 
-#create a testing dataset from meshgrid
-testing = np.vstack([Xscope.ravel(),Yscope.ravel()]).T
-
+# create a testing dataset from meshgrid
+testing = np.vstack([Xscope.ravel(), Yscope.ravel()]).T
 
 
 class Plotter():
-    
     def main(self):
 
-        #plot the vectors
+        # plot the vectors
         self.plot_chart("Vectors")
-        
-        #plot the clusters iteration 
-        for i in [1,2,10]:            
-            prediction,centers  = self.predictKMeans(i)
+
+        # plot the clusters iteration
+        for i in [1, 2, 10]:
+            prediction, centers = self.predictKMeans(i)
             predictionReshaped = prediction.reshape(Xscope.shape)
-            self.plot_chart("Iteration_"+str(i), predictionReshaped, centers)
-    
+            self.plot_chart("Iteration_" + str(i), predictionReshaped, centers)
+
     def predictKMeans(self, iteration):
-        km =  KMeans(init="random", n_clusters = num_clusters, verbose=1, \
-            n_init=1,max_iter=iteration,random_state=seed)
+        km = KMeans(init="random", n_clusters=num_clusters, verbose=1, \
+                    n_init=1, max_iter=iteration, random_state=seed)
         km.fit(training)
 
         return km.predict(testing), km.cluster_centers_
 
-
-    def plot_chart(self,title,C=None, centers=None):
+    def plot_chart(self, title, C=None, centers=None):
         plt.figure(num=None, figsize=(8, 6))
         plt.clf()
 
-        if C !=None:
+        if C != None:
             plt.pcolormesh(Xscope, Yscope, C, cmap=plt.cm.Blues)
             for i in range(len(centers)):
-                plt.scatter(centers[i][0],centers[i][1],s=10, marker='x', linewidth=2,color='g')
-
+                plt.scatter(centers[i][0], centers[i][1], s=10, marker='x', linewidth=2, color='g')
 
         plt.scatter(xDataset, yDataset, s=3, color='r')
         plt.title(title)
         plt.xlabel("word1")
         plt.ylabel("word2")
         plt.xlim()
-        plt.xlim(Xscope.min(),Xscope.max())
+        plt.xlim(Xscope.min(), Xscope.max())
         plt.ylim(Yscope.min(), Yscope.max())
         plt.autoscale(tight=True)
         plt.grid(True, linestyle='-', color='0.75')
         plt.savefig(os.path.join(CHART_DIR, title))
-        
+
+
 if __name__ == "__main__":
     Plotter().main()
-
