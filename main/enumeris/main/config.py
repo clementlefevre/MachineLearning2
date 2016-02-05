eec375537333c4bdd24c7dc94234dae4925e3edf
clementlefevre <clement.san@gmail.com>
2016-02-05 21:31:23 +0100

    Update config.py

--- main/enumeris/main/config.py
+++ main/enumeris/main/config.py
@@ -1,3 +1,3 @@
 __author__ = 'ThinkPad'
 
-DB_URL = "postgresql+pg8000://postgres:clement2014@localhost:5432/DWE_MAUSFRERES_2012"
+DB_URL = "postgresql+pg8000://postgres:xxxx@localhost:5432/DWE_MAUSFRERES_2012"
